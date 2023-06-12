1efd8f0941c11ee9bd14952718812a8c7f0ae911
Austin Obnillas <austinobnillas@gmail.com>
2023-06-12 12:02:50 -0700

    Completed Putting It Together Assignment

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PersonCard = (props) => {
+    const [state, setState] = useState({
+        age: props.age
+    });
+    const updateAge = () => {
+        setState({
+            age: state.age + 1
+        });
+    }
     return (
         <div>
             <h1>{props.lastName}, {props.firstName}</h1>
-            <p>Age: {props.age}</p>
+            <p>Age: {state.age}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick={ updateAge }>Birthday Button for {props.firstName} {props.lastName}</button>
         </div>
     );
 }
